f7779265868a4176a1d5698dbfe06d816043095a
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-09-21 23:33:41 +0530

    Add comprehensive examples and explanations for JavaScript functions, including declarations, expressions, arrow functions, IIFEs, async functions, and callbacks

---
+++ 5_Loops/index.js
@@ -0,0 +1,47 @@
+
+const obj = { name: "John", age: 30, city: "New York" };
+
+for(let key in obj) {
+    console.log(`${key} : ${obj[key]}`);
+}
+
+obj.name = "sanket"; // changing the value of name key
+console.log(obj.name); // sanket
+
+//# For...of
+
+let arr = [10, 20, 30, 40, 50];
+
+for(let val of arr) {
+    console.log(val); // 10, 20, 30, 40, 50
+}
+
+let len = arr.length;
+delete arr[2];
+console.log(arr); // [ 10, 20, <1 empty item>, 40, 50 ]
+
+arr[2] = delete arr[2];
+console.log(arr); // [ 10, 20, true, 40, 50 ]
+
+arr[7] = 70;
+console.log(arr); // [ 10, 20, true, 40, 50, <2 empty items>, 70 ]
+
+console.log(arr.length); // 8
+
+//# ForEach
+
+let forEachArray = [1, 2, 3, 4, 5];
+
+forEachArray.forEach((ele , index) => {
+    return console.log(`Element at index ${index} is ${ele}`);
+})
+
+//# Map
+let mapArray = [1, 2, 3, 4, 5];
+
+let newMap = mapArray.map((ele) => {
+    return ele * 2;
+})
+
+console.log(newMap); // [ 2, 4, 6, 8, 10 ]
+
